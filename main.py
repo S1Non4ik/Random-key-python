080e46fff7649b9bda31a7cf4d3f6598ffe1ea9d
S1Non_ <gooldcrash@gmail.com>
2024-01-22 23:07:31 +0400

    yepee

---
+++ main.py
@@ -0,0 +1,10 @@
+import secrets
+import string
+
+alphabet = string.ascii_letters + string.digits
+a = ''.join(secrets.choice(alphabet) for i in range(5))
+b = ''.join(secrets.choice(alphabet) for i in range(5))
+c = ''.join(secrets.choice(alphabet) for i in range(5))
+print(str(a),'-',(b),'-',(c))
+
+
