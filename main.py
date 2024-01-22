5b3f977cfbe4ea85682b91772a0d3344d9512ef8
S1Non_ <64257552+S1Non4ik@users.noreply.github.com>
2024-01-22 23:09:39 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ alphabet = string.ascii_letters + string.digits
 a = ''.join(secrets.choice(alphabet) for i in range(5))
 b = ''.join(secrets.choice(alphabet) for i in range(5))
 c = ''.join(secrets.choice(alphabet) for i in range(5))
-print(str(a),'-',(b),'-',(c))
+print(str(a),'-',str(b),'-',str(c))
 
 
